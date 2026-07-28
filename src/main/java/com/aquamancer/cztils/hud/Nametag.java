d09bfeefe165438fd13b6c8cde53be2fe72ccec2
aquamancer <aquamancer90@gmail.com>
2026-07-28 17:33:04 -0400

    begin nametag string formatting

--- src/main/java/com/aquamancer/cztils/hud/Nametag.java
+++ src/main/java/com/aquamancer/cztils/hud/Nametag.java
@@ -1,19 +1,57 @@
 package com.aquamancer.cztils.hud;
 
+import com.aquamancer.cztils.Cztils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Nametag extends HudElement {
+    private enum StringType { LITERAL, PLACEHOLDER }
+    private enum PlaceholderType {
+        NAME("{name}"),
+        SPEC("{tree}"),
+        HP("{hp}"),
+        HP_MAX("{hpmax}"),
+        GRAVE("{grave}");
+
+        private static final Map<String, PlaceholderType> FROM_STRING =
+                Arrays.stream(values())
+                        .collect(Collectors.toUnmodifiableMap(
+                                PlaceholderType::getPlaceholder,
+                                Function.identity()
+                        ));
+
+        private final String placeholder;
+
+        PlaceholderType(String placeholder) {
+            this.placeholder = placeholder;
+        }
+
+        private String getPlaceholder() {
+            return this.placeholder;
+        }
+
+        private Optional<PlaceholderType> fromString(String string) {
+            return Optional.ofNullable(FROM_STRING.get(string));
+        }
+    }
+
     private static final char HEART = '♥';
 
-    private String name = "";
-    private String spec = "";
+    private Text name = Text.literal("");
+    private Text spec = Text.literal("");
     private double hp, hpMax, graveTimer;
-    private String hpString = "";
-    private String hpMaxString = "";
-    private MutableText text = Text.literal("");
+    private Text hpMaxTest = Text.literal("");
+    private Text hpMaxText = Text.literal("");
+
+    private List<StringType> formatOrder = new ArrayList<>();
+    private List<String> literals = new ArrayList<>();
+    private List<String> placeholders = new ArrayList<>();
 
     public Nametag(int x, int y) {
         super(x, y);
@@ -23,6 +61,10 @@ public class Nametag extends HudElement {
         this.hpString = String.valueOf(Math.round(this.hp * 10) / 10.0);
         this.hpMaxString = String.valueOf(Math.round(this.hpMax * 10) / 10.0);
         this.text = Text.literal(name).append(" (").append(spec).append(", ").append(String.valueOf(graveTimer)).append("s): ").append(this.hpString).append("/").append(this.hpMaxString);
+    }
+
+    public Nametag setFormat(String formatString) {
+
     }
 
     public Nametag setName(String name) {
